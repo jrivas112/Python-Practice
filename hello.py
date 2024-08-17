740f9ea50fcbbec794efe7e98b67b2c7664e0486
jrivas112 <147008410+jrivas112@users.noreply.github.com>
2024-08-17 16:27:34 -0700

    first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+name = input("What's your name? ")
+
+#remove whitespaces from str and capitalize first letter
+
+#Remove whitespaces from str and capitalize first letter of every word\
+name = name.strip().title()
+
+#implement this right into the input
+name = input("What's your name? ").strip().title()
+
+
+print(f"Hello, {name}")
+
+# Comment
+
+"""
+This is a long comment
+
+"""
